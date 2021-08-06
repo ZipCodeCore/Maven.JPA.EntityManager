3926a33818ef01d6e7353e894b296420945b7899
jgiroso <jrgiroso@gmail.com>
2021-08-06 13:28:47 -0400

    created artist repository and main

---
+++ src/main/java/main/MainApplication.java
@@ -0,0 +1,86 @@
+package main;
+
+import entities.Artist;
+import services.ArtistRepository;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.StringJoiner;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SQLConnect.registerJDBCDriver();
+        Connection mysqlDbConnection = SQLConnect.getConnection("mysql");
+        ArtistRepository artistRepository = new ArtistRepository(mysqlDbConnection);
+        executeStatement(mysqlDbConnection, "DROP DATABASE IF EXISTS artdb;");
+        executeStatement(mysqlDbConnection, "CREATE DATABASE IF NOT EXISTS artdb;");
+        executeStatement(mysqlDbConnection, "USE artdb;");
+        executeStatement(mysqlDbConnection, new StringBuilder()
+                .append("CREATE TABLE IF NOT EXISTS artdb.artists(")
+                .append("id int auto_increment primary key,")
+                .append("name text not null,")
+                .append("birthplace text not null,")
+                .append("birth_year int;")
+                .toString());
+        executeStatement(mysqlDbConnection, new StringBuilder()
+                .append("CREATE TABLE IF NOT EXISTS artdb.paintings(")
+                .append("id int auto_increment primary key,")
+                .append("title text not null,")
+                .append("medium text not null,")
+                .append("artist text not null;")
+                .toString());
+
+        artistRepository.create(new Artist(1L, "Edward Hopper", "New York", 1882));
+        pokemonRepository.create(new Painting(1L, "Nighthawks", "oil", "Edward Hopper"));
+        System.out.println(artistRepository.findAll());
+
+    }
+
+    static ResultSet executeQuery(Connection connection, String sqlQuery) {
+        try {
+            Statement statement = getScrollableStatement(connection);
+            return statement.executeQuery(sqlQuery);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    static void printResults(ResultSet resultSet) {
+        try {
+            for (int rowNumber = 0; resultSet.next(); rowNumber++) {
+                String firstColumnData = resultSet.getString(1);
+                String secondColumnData = resultSet.getString(2);
+                String thirdColumnData = resultSet.getString(3);
+                System.out.println(new StringJoiner("\n")
+                        .add("Row number = " + rowNumber)
+                        .add("First Column = " + firstColumnData)
+                        .add("Second Column = " + secondColumnData)
+                        .add("Third column = " + thirdColumnData));
+            }
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    static void executeStatement(Connection connection, String sqlStatement) {
+        try {
+            Statement statement = getScrollableStatement(connection);
+            statement.execute(sqlStatement);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    static Statement getScrollableStatement(Connection connection) {
+        int resultSetType = ResultSet.TYPE_SCROLL_INSENSITIVE;
+        int resultSetConcurrency = ResultSet.CONCUR_READ_ONLY;
+        try { // scrollable statements can be iterated more than once without closing
+            return connection.createStatement(resultSetType, resultSetConcurrency);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+}
---
+++ src/main/java/services/ArtistRepository.java
@@ -0,0 +1,86 @@
+package services;
+
+import entities.Artist;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArtistRepository implements Repository{
+    private Connection connection;
+
+    public ArtistRepository(Connection connection) {
+        this.connection = connection;
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void create(Artist artist) {
+        executeStatement(String.format(new StringBuilder()
+                        .append("INSERT INTO potions.potionsTable(")
+                        .append("id, name, birthplace, birth_year)")
+                        .append("VALUES (%s, '%s', '%s', %s);")
+                        .toString(),
+                artist.getId(),
+                artist.getName(),
+                artist.getBirthPlace(),
+                artist.getBirthYear()));
+    }
+
+    public List<Artist> findAll() {
+        ResultSet resultSet = executeQuery("SELECT * FROM artdb.artists;");
+        List<Artist> list = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                String id = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                String birthplace = resultSet.getString(3);
+                String birthyear = resultSet.getString(4);
+                list.add(new Artist(
+                        Long.parseLong(id),
+                        name,
+                        birthplace,
+                        Integer.parseInt(birthyear)));
+            }
+        } catch (SQLException throwables) {
+            throw new RuntimeException(throwables);
+        }
+        return list;
+    }
+
+    public Artist findById(Long id) {
+        return findAll()
+                .stream()
+                .filter(artist -> artist.getId().equals(id))
+                .findAny()
+                .get();
+    }
+
+    public void update(Long id, Artist newArtistData) {
+        executeStatement(String.format(new StringBuilder()
+                        .append("UPDATE potions.potionsTable ")
+                        .append("SET name = '%s', birthplace = '%s', birth_year = %s WHERE id = %s;")
+                        .toString(),
+                newArtistData.getName(),
+                newArtistData.getBirthPlace(),
+                newArtistData.getBirthYear(),
+                id));
+    }
+
+    public void delete(Long id) {
+        Artist artist = findById(id);
+        executeStatement(String.format(new StringBuilder()
+                        .append("DELETE FROM artdb.artists WHERE id = %s")
+                        .toString(),
+                id));
+        System.out.printf("%s has been deleted", artist.toString());
+    }
+
+    public void delete(Artist artist) {
+        Long id = artist.getId();
+        delete(id);
+    }
+}
